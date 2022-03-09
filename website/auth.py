2f57e8bbdc28f899d5cad1bc5d01e41f0e3b275c
huynguyen2741 <huynguyen2741@yahoo.com>
2022-03-09 17:48:34 -0500

    add Course

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,45 @@
 from flask import Blueprint, render_template, request, flash
 from . import db
-from .models import Student
-from flask_login import login_user, logout_user, current_user
+from .models import Student, Course
+from flask_login import login_user, logout_user, current_user, login_required
 
 
 auth = Blueprint('auth', __name__)
+isAuthenticated = False
+
+
+# @login_required
+@auth.route('/logout')
+def logout():
+    logout_user()
+    return render_template('index.html')
+
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    student = Student.query.filter_by(username=username).first()
+    if student:
+        if password == student.password:
+            # login_user(student)
+            isAuthenticated = True
+            flash('login success')
+            # render_template("index.html", isAuthenticated=True,
+            #                 username=username, password=password)
+        else:
+            flash('wrong password')
+    else:
+        flash('No user found')
+    return render_template("login.html", isAuthenticated=True,
+                           username=username, password=password)
 
 
 @auth.route('/')
 def home():
-    return render_template("index.html")
-    # return "<h1>hi</h1>"
+    courses = Course.query.filter().all()
+    return render_template("index.html", courses=courses)
 
 
 @auth.route('/sign_up', methods=['GET', 'POST'])
@@ -21,7 +50,10 @@ def sign_up():
         password2 = request.form.get('password2')
         # role = request.form.get('role')
 
-        if len(username) < 2:
+        # check if student exist
+        if Student.query.filter_by(username=username).first():
+            flash("Student exist", category='error')
+        elif len(username) < 2:
             flash("username can\'t be less than 2 characters", category='error')
         elif password1 != password2:
             flash("Password does not match", category='error')
@@ -31,5 +63,9 @@ def sign_up():
             new_student = Student(username=username, password=password1)
             db.session.add(new_student)
             db.session.commit()
+            # login_user(new_student, remember=True)
+            isAuthenticated = True
             flash('Sign up success', category='success')
+            return render_template("index.html", isAuthenticated=True,
+                                   username=username, password=password1)
     return render_template("sign_up.html")
--- website/models.py
+++ website/models.py
@@ -16,6 +16,7 @@ student_course = db.Table('student_course',
 class Course (db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(150))
+    section = db.Column(db.Integer)
 
 
 class Student (db.Model):
@@ -23,5 +24,5 @@ class Student (db.Model):
     username = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     # role = db.Column(db.String(150))
-    taking = db.relation(
+    taking = db.relationship(
         'Course', secondary=student_course, backref='students')
